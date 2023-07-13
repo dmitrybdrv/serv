85178634813cf32c11b0b5695547fd6061d1c0fa
dmitrybdrv <incubator-it@protonmail.com>
2023-07-13 16:59:34 +0300

    CORS fix3

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,16 @@ dotenv.config()
 const app = express()
 const PORT = 5000
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://gilletteopt.ru/unsubscribe-page/:id');
+    res.header('Access-Control-Allow-Origin', 'https://gilletteopt.ru/');
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 
 
 const DBUrl = process.env.DB_HOST || ''
