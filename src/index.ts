73e653308ddc39ce5ec9e95a2d8a748ccdc234da
dmitrybdrv <incubator-it@protonmail.com>
2023-07-13 16:56:44 +0300

    CORS fix2

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://gilletteopt.ru');
+    res.header('Access-Control-Allow-Origin', 'https://gilletteopt.ru/unsubscribe-page/:id');
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
