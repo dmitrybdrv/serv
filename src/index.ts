f61aa672cd59968d359c36e5204bb052cad72f42
dmitrybdrv <incubator-it@protonmail.com>
2023-07-13 16:53:15 +0300

    CORS fix

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://gilletteopt.ru');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
